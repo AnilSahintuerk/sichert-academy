4d57da28a66bdb9860ff69be7a7c00e491eeacfc
AnilSahintuerk <sahintuerk.anil@gmail.com>
2021-12-21 13:14:50 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Preview from "./components/Preview";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#245FA7" },
+    secondary: { main: "#555F71" },
+    tertiary: { main: "#6E5675", contrastText: "#fff" },
+    error: { main: "#BA1B1B" },
+    background: { main: "#FDFBFF", contrastText: "#1B1B1D" },
+    sichert: { main: "#003063", contrastText: "#fff" },
+    primary_container: { main: "#D4E3FF", contrastText: "#001B3D" },
+    secondary_container: { main: "#D9E3F8", contrastText: "#121C2B" },
+    tertiary_container: { main: "#F8D8FE", contrastText: "#27132F" },
+    error_container: { main: "#FFDAD4", contrastText: "#410001" },
+    surface_variant: { main: "#E0E2EB", contrastText: "#43474F" },
+    neutral: { main: "#74777F", contrastText: "#fff" },
+    white: { main: "#fff" },
+    black: { main: "#000" },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Preview />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Preview />
+      </div>
+    </ThemeProvider>
   );
 }
 
