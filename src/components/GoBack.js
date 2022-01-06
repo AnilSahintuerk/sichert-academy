37437fed83e760bd164ad6df095135715f3ce718
AnilSahintuerk <sahintuerk.anil@gmail.com>
2022-01-06 21:24:36 +0100

    Go back functionality added

--- src/components/GoBack.js
+++ src/components/GoBack.js
@@ -1,18 +1,23 @@
 import { Typography, Stack, IconButton } from "@mui/material";
-
+import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function GoBack(props) {
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate(-1);
+  };
   return (
     <Stack
       direction="row"
       alignItems="center"
       sx={{ gap: "4px", marginTop: "24px" }}
     >
-      <IconButton onClick={props.onBack}>
+      <IconButton onClick={clickHandler}>
         <ArrowBackIcon fontSize="large" sx={{ color: "#886E8F" }} />
-        <Typography sx={{ color: "#886E8F" }}>Zurück</Typography>
       </IconButton>
+      <Typography sx={{ color: "#886E8F" }}>Zurück</Typography>
     </Stack>
   );
 }
