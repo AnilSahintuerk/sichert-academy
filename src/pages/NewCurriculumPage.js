dbe5fd5aabe17f48621e3ac2600fd8c9216456b8
AnilSahintuerk <sahintuerk.anil@gmail.com>
2022-01-07 13:46:39 +0100

    forgot to push this

--- src/pages/NewCurriculumPage.js
+++ src/pages/NewCurriculumPage.js
@@ -1,28 +1,60 @@
 import GoBack from "../components/GoBack";
-import { Stack, Box, Container, Typography } from "@mui/material";
+import { Stack, Box, Typography, TextField, Button } from "@mui/material";
 import VideoCardDrag from "../components/VideoCardDrag";
 
 function NewCurriculumPage(props) {
   return (
-    <Stack spacing={10} sx={{ ml: "24px" }}>
-      <GoBack />
-      <Box sx={{ maxHeight: "100vh", overflow: "scroll" }}>
-        <Stack
-          sx={{
-            maxWidth: "600px",
-            gap: "16px",
-            pb: "128px",
-          }}
-        >
-          <Typography variant="h4" align="center" sx={{ color: "#563E5D" }}>
-            Lehrplan
-          </Typography>
-          {props.lectures.map((lecture) => (
-            <VideoCardDrag lecture={lecture} />
-          ))}
+    <Box sx={{ display: "flex" }}>
+      <Stack
+        spacing={10}
+        sx={{ p: "0 24px", background: "#E3E2E6", height: "100vh" }}
+      >
+        <GoBack />
+        <Box sx={{ maxHeight: "100vh", overflow: "scroll" }}>
+          <Stack
+            sx={{
+              maxWidth: "600px",
+              gap: "16px",
+              pb: "128px",
+            }}
+          >
+            <Typography variant="h4" align="center" sx={{ color: "#563E5D" }}>
+              Neuer Lehrplan
+            </Typography>
+            {props.lectures.map((lecture) => (
+              <VideoCardDrag lecture={lecture} />
+            ))}
+          </Stack>
+        </Box>
+      </Stack>
+
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Stack spacing={5} sx={{ mb: "20%", width: "40%" }}>
+          <TextField
+            required
+            id="outlined-required"
+            label="Lehrplan Name"
+          ></TextField>
+          <TextField id="outlined-required" label="Klasse"></TextField>
+          <TextField
+            id="outlined-required"
+            label="Nachricht"
+            multiline
+            rows={6}
+          ></TextField>
+          <Button variant="contained" color="sichert">
+            Lehrplan erstellen
+          </Button>
         </Stack>
       </Box>
-    </Stack>
+    </Box>
   );
 }
 export default NewCurriculumPage;
